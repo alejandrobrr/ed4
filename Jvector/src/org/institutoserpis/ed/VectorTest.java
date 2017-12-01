a5e71ca672ef6d40f2b04d643d713bc3687a7ee7
alejandrobrr <31982913+alejandrobrr@users.noreply.github.com>
2017-12-01 08:56:59 +0100

    Add files via upload

---
+++ Jvector/src/org/institutoserpis/ed/VectorTest.java
@@ -0,0 +1,10 @@
+package org.institutoserpis.ed;
+
+public class VectorTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Jvector/src/org/institutoserpis/ed/vector.java
@@ -0,0 +1,22 @@
+package org.institutoserpis.ed;
+
+public class vector {
+
+	public static void main(String[] args) {
+
+		int [] v = new int[] {9,15,7,12,6};
+	for (int index = 0 ; index <5; index++)
+		System.out.printf("v,[%s]=%s\n ", index, v[index]);
+		System.out.println();
+		
+		int value = 12; 
+		int position = index0f(v,value);
+		System.out.println("Posicion= " + position);
+
+	}
+public static int index0f(int [] v, int value) {
+	 
+	return -1;
+}
+}
+
